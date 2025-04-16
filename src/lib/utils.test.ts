efa660b7df8443a8ededdf5e55bb900a42d2f037
chetan bhat <chetdbhat@gmail.com>
2025-04-16 17:30:52 +0000

    updated and added unit tests

---
+++ src/lib/utils.test.ts
@@ -0,0 +1,47 @@
+import { cn } from './utils';
+
+describe('cn', () => {
+  it('should return an empty string when no arguments are passed', () => {
+    expect(cn()).toEqual('');
+  });
+
+  it('should return the same string when a single string argument is passed', () => {
+    expect(cn('text-red-500')).toEqual('text-red-500');
+  });
+
+  it('should merge multiple string arguments', () => {
+    expect(cn('text-red-500', 'bg-blue-500')).toEqual(
+      'text-red-500 bg-blue-500',
+    );
+  });
+
+  it('should handle conditional arguments correctly', () => {
+    expect(cn('text-red-500', false && 'bg-blue-500')).toEqual(
+      'text-red-500',
+    );
+    expect(cn('text-red-500', true && 'bg-blue-500')).toEqual(
+      'text-red-500 bg-blue-500',
+    );
+  });
+
+  it('should mix tailwind utility classes with other classes', () => {
+    expect(cn('text-red-500', 'my-custom-class')).toEqual(
+      'text-red-500 my-custom-class',
+    );
+  });
+
+  it('should correctly merge complex class strings', () => {
+    expect(
+      cn(
+        'text-red-500 font-bold',
+        'text-blue-500',
+        'hover:text-green-500',
+        'p-4',
+        'm-2',
+        'hover:underline',
+      ),
+    ).toEqual(
+      'text-blue-500 font-bold hover:text-green-500 p-4 m-2 hover:underline',
+    );
+  });
+});
